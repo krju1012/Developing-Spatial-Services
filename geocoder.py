2949fe53b385fcd86f014ff8b783d7d49501424d
krju1012 <julian.krauth@eifer.uni-karlsruhe.de>
2017-08-04 22:03:06 +0200

    Add files via upload

--- geocoder.py
+++ geocoder.py
@@ -4,68 +4,12 @@ import sqlite3
 from flask import g
 
 app = Flask(__name__)
-DATABASE= '/media/sf_shared_folder/my_bremen_osm_1.sqlite'
-
-
-def generate_sql(arg1, arg2):
-  
-    sql_command='''SELECT node_id, adress,(ST_Distance(MakePoint({0}, {1}), geom)) /   1000 AS dist_km
-    FROM my_osm_new_adresses as a
-    where ST_Contains(ST_Buffer(MakePoint({0}, {1}),10000),geom)
-     AND  a.ROWID IN (
-     SELECT ROWID
-     FROM SpatialIndex
-     WHERE f_table_name = 'my_osm_new_adresses'
-     AND search_frame = ST_Buffer(MakePoint({0}, {1}),10000))
-     order by (ST_Distance(MakePoint({0}, {1}), geom))  limit 1
-    ;'''.format(arg1, arg2) 
-    print sql_command
-    return sql_command
-
-@app.before_request
-def dataconnect():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    g._database.enable_load_extension(True)
-    g._database.load_extension('/usr/local/lib/mod_spatialite')
-    print "Database succesfully connected"
-
-
-
-@app.teardown_request
-def dataconnect_close(exception):
-	db = getattr(g, '_database', None)
-	if db is not None:
-		db.close()
-
-@app.route('/db')
-def query():
-    cur = g._database.cursor()
-    cur.execute('''select * from osm_nodes limit 10''')
-    print cur.fetchall()
-    return 'connected'
-   
-@app.route('/geocode')
-def calc_distance():
-  frompt = request.args.get('from')
-  #topt = request.args.get('to')
-  lat1,lon1 = frompt.split(",")
-  print lat1, lon1
-  #lat2,lon2 = topt.split(",")
-  cur2 = g._database.cursor()
-  cur2.execute(generate_sql(lat1,lon1))
-  
-  results= cur2.fetchall()
-  print results
-  return str(results)
-
 
 @app.route("/")
 @app.route("/<user>")
 def template_test(user=None):
-    return render_template('index.html', user=user)
+  return render_template('index.html', user=user)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
--- scripts.js
+++ scripts.js
@@ -1,81 +1,110 @@
 
 <!-- Map preferences: -->
-	var mymap = L.map('mapid',{
-		center: [48.7, 7],
-		zoom: 5,
-		zoomControl:false })
+  var mymap = L.map('mapid',{
+    center: [48.7, 7],
+    zoom: 5,
+    zoomControl:false })
 
 
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 18,
-		id: 'mapbox.satellite'
-	}).addTo(mymap);
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    id: 'mapbox.satellite'
+  }).addTo(mymap);
 
-	
+  
 <!-- LatLong-Popup -->
-	var popup = L.popup();
-	var lat, lng;
-	
-    var counter = 0;
-	
-	function onMapClick(e) {
-		lat = e.latlng.lat;
-		lng = e.latlng.lng;
-		var from = turf.point([lat, lng]);
-		var to = turf.point([48, 8]);
-		var distance = turf.distance(from, to);
-		var shortdistance = parseInt(distance.toFixed(2));
-		popup
-			.setLatLng(e.latlng)
-			.setContent(shortdistance + " km entfernt")
-			.openOn(mymap);
-		var add = (function () {
-			
-			return function () {
-			    console.log(counter)
-				return counter += shortdistance;
-			
-			}
-})();
-		
-		function myFunction(){
-			document.getElementById("counter").innerHTML = add();
-		}
-		
-		myFunction();
-	}
+  var popup = L.popup();
+  var lat, lng;
+  
+  var counter = 0;
+  currentSlide = n;
+  
+  function onMapClick(e) {  //eher innerhalb function darunter ab onlinequiz
+    lat = e.latlng.lat;
+    lng = e.latlng.lng;
+    var from = turf.point([lat, lng]);
+    var to = turf.point([48, 8]);
+    var distance = turf.distance(from, to);
+    var shortdistance = parseInt(distance.toFixed(2));
+    popup
+      .setLatLng(e.latlng)
+      .setContent(shortdistance + " km entfernt")
+      .openOn(mymap);
+    var add = (function () {
+      
+      return function () {
+          console.log(counter)
+        return counter += shortdistance;
+      
+      }
+    }) ();
+    
+    function myFunction(){
+      document.getElementById("counter").innerHTML = add();
+    }
+    
+    myFunction();
 
-	mymap.on('click', onMapClick);
-	
-// <!-- Fragebogen -->
-// (function() {
-  // const myQuestions = [
-    // {
-      // question: "Who is the strongest?",
-      // answers: {
-        // a: "Superman",
-        // b: "The Terminator",
-        // c: "Waluigi, obviously"
-      // },
-      // correctAnswer: "c"
-    // },
-    // {
-      // question: "What is the best site ever created?",
-      // answers: {
-        // a: "SitePoint",
-        // b: "Simple Steps Code",
-        // c: "Trick question; they're both the best"
-      // },
-      // correctAnswer: "c"
-    // },
-    // {
-      // question: "Where is Waldo really?",
-      // answers: {
-        // a: "Antarctica",
-        // b: "Exploring the Pacific Ocean",
-        // c: "Sitting in a tree",
-        // d: "Minding his own business, so stop asking"
-      // },
-      // correctAnswer: "d"
-    // }
-  // ];
+    //anzeige der nächsten Frage
+    function showNextSlide() {
+      showSlide(currentSlide + 1);
+    }
+  }
+
+  mymap.on('click', onMapClick);
+  mymap.on('click', showNextSlide);
+
+
+//onlinequiz
+
+(function() {
+  const myQuestions = [
+    {
+      question: "Where is the Heidelberger Schloss located?",
+      correctAnswer: "49.411,8.715"
+    },
+    {
+      question: "Where does the Queen live?",
+      correctAnswer: "51.501,-0.142"
+    }
+  ];
+
+  function buildQuiz(){
+    // we'll need a place to store the HTML output
+    const output = [];
+
+    // for each question...
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+
+        // we'll want to store the list of answer choices
+        const answers = [];
+
+        for (i in currentQuestion.answers){
+          answers.push(currentQuestion.answers[i])
+        }
+      }
+    output.push(
+        `<div class="slide">
+           <div class="question"> ${currentQuestion.question} </div>
+         </div>`
+      );
+    });
+
+    quizContainer.innerHTML = output;  //????quizContainer.innerHTML = output();
+  }
+
+  function showSlide(n) {
+    slides[currentSlide].classList.remove("active-slide");
+    slides[n].classList.add("active-slide");
+  }
+  
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;  //muss außerhalb der Klickfunction sein da sonst immer wieder neu angefangen wird
+
+
+
+
+  buildQuiz();
+  
+const quizContainer = document.getElementById('quiz');
