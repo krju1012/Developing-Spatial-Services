6d685d721a7d5be78b09d3d5beeedbca249efa95
krju1012 <julian.krauth@eifer.uni-karlsruhe.de>
2017-08-11 18:59:16 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,110 +1,87 @@
 
 <!-- Map preferences: -->
-  var mymap = L.map('mapid',{
-    center: [48.7, 7],
-    zoom: 5,
-    zoomControl:false })
+ var mymap = L.map('mapid',{
+   center: [48.7, 7],
+   zoom: 5,
+   zoomControl:false })
 
 
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    id: 'mapbox.satellite'
-  }).addTo(mymap);
+ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+   maxZoom: 18,
+   id: 'mapbox.satellite'
+ }).addTo(mymap);
 
-  
+ 
 <!-- LatLong-Popup -->
-  var popup = L.popup();
-  var lat, lng;
-  
-  var counter = 0;
-  currentSlide = n;
-  
-  function onMapClick(e) {  //eher innerhalb function darunter ab onlinequiz
-    lat = e.latlng.lat;
-    lng = e.latlng.lng;
-    var from = turf.point([lat, lng]);
-    var to = turf.point([48, 8]);
-    var distance = turf.distance(from, to);
-    var shortdistance = parseInt(distance.toFixed(2));
-    popup
-      .setLatLng(e.latlng)
-      .setContent(shortdistance + " km entfernt")
-      .openOn(mymap);
-    var add = (function () {
-      
-      return function () {
-          console.log(counter)
-        return counter += shortdistance;
-      
-      }
-    }) ();
-    
-    function myFunction(){
-      document.getElementById("counter").innerHTML = add();
-    }
-    
-    myFunction();
+ var popup = L.popup();
+ var lat, lng;
+ 
+ <!-- Clickcounter for pointdistance -->
+ var counter = 0;
 
-    //anzeige der nächsten Frage
-    function showNextSlide() {
-      showSlide(currentSlide + 1);
-    }
-  }
+  <!-- Clickcounter for questioncount, works in firebug-->
+ var questioncounter = 0;
+ 
+<!-- Quizquestions, erster Eintrag leer da bei 0 clicks noch kein print?!?! -->
+var myquestions = [
+  "",
+  "Wo steht das Heidelberger Schloss?",
+  "Wo ist die Queen beheimatet?",
+];
 
-  mymap.on('click', onMapClick);
-  mymap.on('click', showNextSlide);
+ function onMapClick(e) {
+   questioncounter +=1;
+   lat = e.latlng.lat;
+   lng = e.latlng.lng;
+   var from = turf.point([lat, lng]);
+   var to = turf.point([48, 8]);
+   var distance = turf.distance(from, to);
+   var shortdistance = parseInt(distance.toFixed(2));
+   popup
+     .setLatLng(e.latlng)
+     .setContent(shortdistance + " km entfernt")
+     .openOn(mymap);
+   var add = (function () {
+     
+     return function () {
+         console.log(counter)
+       return counter += shortdistance;
+     
+     }
+})();
+   
 
 
-//onlinequiz
 
-(function() {
-  const myQuestions = [
-    {
-      question: "Where is the Heidelberger Schloss located?",
-      correctAnswer: "49.411,8.715"
-    },
-    {
-      question: "Where does the Queen live?",
-      correctAnswer: "51.501,-0.142"
-    }
-  ];
 
-  function buildQuiz(){
-    // we'll need a place to store the HTML output
-    const output = [];
+   function myFunction(){
+     document.getElementById("counter").innerHTML = add();
+   }
+   
+   myFunction();
+ }
 
-    // for each question...
-    myQuestions.forEach(
-      (currentQuestion, questionNumber) => {
+ mymap.on('click', onMapClick);
 
-        // we'll want to store the list of answer choices
-        const answers = [];
 
-        for (i in currentQuestion.answers){
-          answers.push(currentQuestion.answers[i])
-        }
-      }
-    output.push(
-        `<div class="slide">
-           <div class="question"> ${currentQuestion.question} </div>
-         </div>`
-      );
+
+ $('#mapid')
+  .click(function() {
+    document
+        .getElementById('question')
+        .innerHTML = myquestions[questioncounter];  
     });
+ 
+ // var q1 = "Wo steht der Eiffelturm?";
+ // var q2 = "An welchem Ort findet das Oktoberfest statt?";
+ // var questioncounter = 0;
 
-    quizContainer.innerHTML = output;  //????quizContainer.innerHTML = output();
-  }
+ // function questionclicks() {
+ //    questioncounter +=1;
+ //    if questioncounter = 0 
+ //      document.getElementById('q1'),
+ //    else if questioncounter = 1
+ //      document.getElementById('q2');
+ // }
 
-  function showSlide(n) {
-    slides[currentSlide].classList.remove("active-slide");
-    slides[n].classList.add("active-slide");
-  }
-  
-  const slides = document.querySelectorAll(".slide");
-  let currentSlide = 0;  //muss außerhalb der Klickfunction sein da sonst immer wieder neu angefangen wird
-
-
-
-
-  buildQuiz();
-  
-const quizContainer = document.getElementById('quiz');
+ // mymap.on('click', questionclicks );
